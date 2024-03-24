e7f054da96d4c15bbb96441559066a789344c28d
MRutkowski00 <m.rutkowski.archviz@gmail.com>
2024-03-24 18:13:59 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,748 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lobby",
+      "name": "Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.019272125109026,
+          "pitch": -0.025920267673521735,
+          "rotation": 0,
+          "target": "2-sala-konferencyjna---parter-"
+        },
+        {
+          "yaw": 1.2554073976681561,
+          "pitch": 0.040738365329501036,
+          "rotation": 0,
+          "target": "1-komunikacja---parter"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-komunikacja---parter",
+      "name": "Komunikacja - parter",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5483569229689973,
+          "pitch": 0.020704221601572925,
+          "rotation": 0,
+          "target": "0-lobby"
+        },
+        {
+          "yaw": -2.643891095904568,
+          "pitch": 0.056344775114125056,
+          "rotation": 0,
+          "target": "3-wc-dla-ozn"
+        },
+        {
+          "yaw": 0.025711501749920984,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-komunikacja---pitro-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sala-konferencyjna---parter-",
+      "name": "Sala konferencyjna - parter ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.22459489643874,
+          "pitch": -0.020321258761699212,
+          "rotation": 0,
+          "target": "0-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wc-dla-ozn",
+      "name": "WC dla OzN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2602944301755485,
+          "pitch": -0.046568820901436325,
+          "rotation": 0,
+          "target": "1-komunikacja---parter"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-komunikacja---pitro-a",
+      "name": "Komunikacja - piętro A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1228116601016946,
+          "pitch": 0.024446214456929738,
+          "rotation": 0,
+          "target": "5-open-space-a"
+        },
+        {
+          "yaw": 0.31359639657950744,
+          "pitch": 0.053578660875773565,
+          "rotation": 0,
+          "target": "7-wc-damskie"
+        },
+        {
+          "yaw": -0.2102967514548677,
+          "pitch": 0.05025895968058869,
+          "rotation": 0,
+          "target": "8-biuro-wiksze-z-oknem"
+        },
+        {
+          "yaw": 0.002571712864920528,
+          "pitch": 0.0539535335363972,
+          "rotation": 0,
+          "target": "9-komunikacja---pitro-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-open-space-a",
+      "name": "Open space A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5025916233684633,
+          "pitch": 0.0020304152119194896,
+          "rotation": 0,
+          "target": "6-open-space-b"
+        },
+        {
+          "yaw": -3.1244691468112684,
+          "pitch": 0.040147038370756505,
+          "rotation": 0,
+          "target": "4-komunikacja---pitro-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-open-space-b",
+      "name": "Open space B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "5-open-space-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-wc-damskie",
+      "name": "WC damskie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5905364743143302,
+          "pitch": 0.02790338750531518,
+          "rotation": 0,
+          "target": "4-komunikacja---pitro-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-biuro-wiksze-z-oknem",
+      "name": "Biuro większe z oknem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6684990714771253,
+          "pitch": 0.06963713306069685,
+          "rotation": 0,
+          "target": "4-komunikacja---pitro-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-komunikacja---pitro-b",
+      "name": "Komunikacja - piętro B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1241176130330075,
+          "pitch": 0.029356885588336823,
+          "rotation": 0,
+          "target": "4-komunikacja---pitro-a"
+        },
+        {
+          "yaw": 0.12231034889068759,
+          "pitch": 0.030244428270266965,
+          "rotation": 0,
+          "target": "11-kantyna---pitro"
+        },
+        {
+          "yaw": 0.5964939742151216,
+          "pitch": 0.02918345378250642,
+          "rotation": 0,
+          "target": "12-sala-konferencyjna---pitro-a"
+        },
+        {
+          "yaw": -1.0881949249278549,
+          "pitch": 0.032056781589218986,
+          "rotation": 0,
+          "target": "10-biuro-mniejsze-z-oknem"
+        },
+        {
+          "yaw": -0.012857876831514403,
+          "pitch": 0.03084827594860151,
+          "rotation": 0,
+          "target": "13-komunikacja---pitro-c"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-biuro-mniejsze-z-oknem",
+      "name": "Biuro mniejsze z oknem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6323076380551367,
+          "pitch": 0.08007672162554513,
+          "rotation": 0,
+          "target": "9-komunikacja---pitro-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-kantyna---pitro",
+      "name": "Kantyna - piętro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.42929367676435604,
+          "pitch": 0.02571633524951622,
+          "rotation": 0,
+          "target": "9-komunikacja---pitro-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-sala-konferencyjna---pitro-a",
+      "name": "Sala konferencyjna - piętro A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2786011619013706,
+          "pitch": 0.08383077967837593,
+          "rotation": 0,
+          "target": "9-komunikacja---pitro-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-komunikacja---pitro-c",
+      "name": "Komunikacja - piętro C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.002571710769458946,
+          "pitch": 0.018000014545883403,
+          "rotation": 0,
+          "target": "9-komunikacja---pitro-b"
+        },
+        {
+          "yaw": 2.7760871476435236,
+          "pitch": 0.04736204433683966,
+          "rotation": 0,
+          "target": "14-biuro-zarzdu"
+        },
+        {
+          "yaw": -0.5559849462555917,
+          "pitch": 0.020844328062599615,
+          "rotation": 0,
+          "target": "15-sala-konferencyjna---pitro-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-biuro-zarzdu",
+      "name": "Biuro zarządu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4984688997288025,
+          "pitch": 0.05292592018950337,
+          "rotation": 0,
+          "target": "13-komunikacja---pitro-c"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-sala-konferencyjna---pitro-b",
+      "name": "Sala konferencyjna - piętro B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0455285143828554,
+          "pitch": 0.0511853045669568,
+          "rotation": 0,
+          "target": "16-biuro-ze-wietlikiem"
+        },
+        {
+          "yaw": -1.6675127213565837,
+          "pitch": 0.055938466975469225,
+          "rotation": 0,
+          "target": "13-komunikacja---pitro-c"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8115838434570781,
+          "pitch": 0.06799544972789384,
+          "title": "W opracowaniu",
+          "text": "0.16 Komunikacja parter B<br>0.17 Jadalnia - parter&nbsp;<br>0.20 Szatnia męska"
+        }
+      ]
+    },
+    {
+      "id": "16-biuro-ze-wietlikiem",
+      "name": "Biuro ze świetlikiem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8396283808160963,
+          "pitch": 0.054575485666951806,
+          "rotation": 0,
+          "target": "15-sala-konferencyjna---pitro-b"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Wykroty_P04",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
